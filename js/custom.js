b4754be44b4689f50b01ea48a33ea880207075ad
Pratikkk <kenipratik9@gmail.com>
2022-12-11 12:17:27 +0530

    finalizing frontend

--- js/custom.js
+++ js/custom.js
@@ -28,13 +28,89 @@
 
     
 /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-let prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  let currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementsByClassName("navbar").style.top = "0";
-  } else {
-    document.getElementsByClassName("navbar").style.top = "-50px";
-  }
-  prevScrollpos = currentScrollPos;
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+//   var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementsByClassName("navbar").style.top = "0";
+//   } else {
+//     document.getElementsByClassName("navbar").style.top = "-50";
+//   }
+//   prevScrollpos = currentScrollPos;
+// }
+
+// (function($){
+//   var duration = 2500;
+//   var current = 1;
+//   var tricker = $('.tricker');
+//   var height = tricker.height();
+//   var number = tricker.children().length;
+//   var first = tricker.children().first();
+  
+//   setInterval(function(){
+//     var interv = current*(-1)*height;
+//     first.css('margin-top',interv + 'px');
+//     if(current == number){
+//       first.css('margin-top','0px');
+//       current = 1;
+//     }else{
+//       current++;
+//     }
+//   },duration);
+// })(jQuery)
+
+var words = document.getElementsByClassName('word');
+var wordArray = [];
+var currentWord = 0;
+
+words[currentWord].style.opacity = 1;
+for (var i = 0; i < words.length; i++) {
+  splitLetters(words[i]);
 }
+
+function changeWord() {
+  var cw = wordArray[currentWord];
+  var nw = currentWord == words.length-1 ? wordArray[0] : wordArray[currentWord+1];
+  for (var i = 0; i < cw.length; i++) {
+    animateLetterOut(cw, i);
+  }
+  
+  for (var i = 0; i < nw.length; i++) {
+    nw[i].className = 'letter behind';
+    nw[0].parentElement.style.opacity = 1;
+    animateLetterIn(nw, i);
+  }
+  
+  currentWord = (currentWord == wordArray.length-1) ? 0 : currentWord+1;
+}
+
+function animateLetterOut(cw, i) {
+  setTimeout(function() {
+		cw[i].className = 'letter out';
+  }, i*80);
+}
+
+function animateLetterIn(nw, i) {
+  setTimeout(function() {
+		nw[i].className = 'letter in';
+  }, 340+(i*80));
+}
+
+function splitLetters(word) {
+  var content = word.innerHTML;
+  word.innerHTML = '';
+  var letters = [];
+  for (var i = 0; i < content.length; i++) {
+    var letter = document.createElement('span');
+    letter.className = 'letter';
+    letter.innerHTML = content.charAt(i);
+    word.appendChild(letter);
+    letters.push(letter);
+  }
+  
+  wordArray.push(letters);
+}
+
+changeWord();
+setInterval(changeWord, 4000);
+
